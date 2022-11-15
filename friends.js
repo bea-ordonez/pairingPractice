c4abf712f1e56077d8873d4b64ad590f0c158746
Barbie Ordonez <ordonezbarbie@gmail.com>
2022-11-14 17:06:14 -0700

    Add places array

--- friends.js
+++ friends.js
@@ -3,3 +3,7 @@ var friends = ["Mary", "Gabe", "Kyle", "Katie", "John"]
 console.log(friends[2])
 console.log(friends[4])
 console.log(friends[0])
+
+var places = ["Paris", "London", "New York", "Fiji", "Tokyo"]
+
+console.log(places.push("Peru", "Seattle"))
